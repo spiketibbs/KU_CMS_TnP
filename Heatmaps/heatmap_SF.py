de54f185abd83440816a45ebb3ad3c6af9dd2a17
Neil Joshi <neiljoshi2009@gmail.com>
2026-07-17 22:06:58 -0500

    Update heatmap_SF.py

--- Heatmaps/heatmap_SF.py
+++ Heatmaps/heatmap_SF.py
@@ -25,7 +25,7 @@ LEPTON_CONFIGS = {
     'Electron': {
         # Modify the excel path to match the correct file for electrons
         'excel_path': f"Fitter/Electrons/{ERA_clean}/{NUM_clean}/sfs_ecol.xlsx",
-        # Changes the pt_labels to '2-4' and '4-7' if 2016-2018, or '2-5' and '5-7' if 2022-2023
+        # Changes the pt_labels to '2-4' and '4-7' if 2016-2018, or '2-5' and '5-7' if 2022-2026
         'pt_labels': ['2-4', '4-7', '7-10', '10-20', '20-45', '45-75', '75-500']
         if ERA in ["2016 preVFP", "2016 post VFP", "2017", "2018"]
         else ['2-5', '5-7', '7-10', '10-20', '20-45', '45-75', '75-500'],
